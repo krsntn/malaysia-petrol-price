116ecc17081c4000d76ac673f68e4fd7a99aeef4
krsntn <kanahan0110@gmail.com>
2023-08-17 14:48:01 +0800

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -9,55 +9,81 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
+
+function initialData() {
+  const data = {
+    from: "-",
+    through: "-",
+    ron95: ["-", "RM 0"],
+    ron97: ["-", "RM 0"],
+    diesel: ["-", "RM 0"],
+  };
+
+  return Array(10)
+    .fill()
+    .map(() => data);
+}
 
 function App() {
-  const [data, setData] = useState([
-    {
-      from: "-",
-      through: "-",
-      ron95: ["-", "RM 0"],
-      ron97: ["-", "RM 0"],
-      diesel: ["-", "RM 0"],
-    },
-  ]);
+  const [data, setData] = useState(() => initialData());
+
   useEffect(() => {
     fetch("https://dulcet-kringle-c21419.netlify.app/.netlify/functions/petrol")
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
   return (
-    <>
-      <div className="max-w-2xl m-auto">
-        <div className="grid grid-cols-3 gap-4">
-          <Card className="bg-yellow-500 text-white">
-            <CardHeader>
-              <CardTitle>RON 95</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{data[0].ron95[0]}</p>
-            </CardContent>
-          </Card>
-          <Card className="bg-green-700 text-white">
-            <CardHeader>
-              <CardTitle>RON 97</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{data[0].ron97[0]}</p>
-            </CardContent>
-          </Card>
-          <Card className="bg-black text-white">
-            <CardHeader>
-              <CardTitle className="break-all">DIESEL</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{data[0].diesel[0]}</p>
-            </CardContent>
-          </Card>
-        </div>
+    <div className="h-full min-h-[100dvh] p-4 max-w-2xl m-auto flex flex-col justify-around">
+      <div className="grid grid-cols-3 gap-4">
+        <Card className="bg-yellow-500 text-white">
+          <CardHeader>
+            <CardTitle>RON 95</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              {data[0].ron95[0] === "-" ? (
+                <Skeleton className="h-4 w-full" />
+              ) : (
+                data[0].ron95[0]
+              )}
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="bg-green-700 text-white">
+          <CardHeader>
+            <CardTitle>RON 97</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              {data[0].ron97[0] === "-" ? (
+                <Skeleton className="h-4 w-full" />
+              ) : (
+                data[0].ron97[0]
+              )}
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="bg-black text-white">
+          <CardHeader>
+            <CardTitle className="break-all">DIESEL</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>
+              {data[0].diesel[0] === "-" ? (
+                <Skeleton className="h-4 w-full" />
+              ) : (
+                data[0].diesel[0]
+              )}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
 
-        <div className="h-[50px]" />
-
+      <ScrollArea className="h-[50dvh] md:h-[70dvh] w-full">
         <Table>
           <TableCaption>Malaysia petrol fuel price history.</TableCaption>
           <TableHeader>
@@ -74,13 +100,26 @@ function App() {
               return (
                 <TableRow key={i}>
                   <TableCell className="text-center align-text-top">
-                    {x.from}
+                    {x.from === "-" ? (
+                      <Skeleton className="h-4 w-full" />
+                    ) : (
+                      x.from
+                    )}
                   </TableCell>
                   <TableCell className="text-center align-text-top">
-                    {x.through}
+                    {x.through === "-" ? (
+                      <Skeleton className="h-4 w-full" />
+                    ) : (
+                      x.through
+                    )}
                   </TableCell>
                   <TableCell className="text-center align-text-top font-medium">
-                    {x.ron95[0]}
+                    {x.ron95[0] === "-" ? (
+                      <Skeleton className="h-4 w-full" />
+                    ) : (
+                      x.ron95[0]
+                    )}
+
                     {x.ron95[1] !== "RM 0" && (
                       <>
                         <br />
@@ -89,7 +128,12 @@ function App() {
                     )}
                   </TableCell>
                   <TableCell className="text-center align-text-top font-medium">
-                    {x.ron97[0]}
+                    {x.ron97[0] === "-" ? (
+                      <Skeleton className="h-4 w-full" />
+                    ) : (
+                      x.ron97[0]
+                    )}
+
                     {x.ron97[1] !== "RM 0" && (
                       <>
                         <br />({x.ron97[1]})
@@ -97,7 +141,11 @@ function App() {
                     )}
                   </TableCell>
                   <TableCell className="text-center align-text-top font-medium">
-                    {x.diesel[0]}
+                    {x.diesel[0] === "-" ? (
+                      <Skeleton className="h-4 w-full" />
+                    ) : (
+                      x.diesel[0]
+                    )}
                     {x.diesel[1] !== "RM 0" && (
                       <>
                         <br />({x.diesel[1]})
@@ -109,12 +157,12 @@ function App() {
             })}
           </TableBody>
         </Table>
+      </ScrollArea>
 
-        <div className="text-xs my-10">
-          © 2020, Built by <a href="https://dev.krsn.xyz">Karson</a>
-        </div>
+      <div className="text-xs mt-10">
+        © 2020, Built by <a href="https://dev.krsn.xyz">Karson</a>
       </div>
-    </>
+    </div>
   );
 }
 
